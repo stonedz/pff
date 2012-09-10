b7d75ca98199037bce78e4197f7204e4c4564ea6
Paolo Fagni <paolo.fagni@gmail.com>
2012-09-10 12:38:58 +0200

    Fixed dependancy

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -9,7 +9,7 @@
 
 //require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'lib' . DS . 'autoload.php');
-require_once (ROOT . DS . 'lib' .DS . 'vendor' . DS . '.composer' . DS. 'autoload.php');
+//require_once (ROOT . DS . 'lib' .DS . 'vendor' . DS . '.composer' . DS. 'autoload.php');
 require_once (ROOT . DS . 'app' . DS . 'autoload.php');
 require_once (ROOT . DS . 'lib' . DS . 'App.php');
 require_once (ROOT . DS . 'lib' . DS . 'shared.php');
