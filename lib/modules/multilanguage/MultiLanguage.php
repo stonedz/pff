5493eba568c5b5ff2a80b14a8da7126a21e02d17
Paolo Fagni <paolo.fagni@gmail.com>
2012-09-28 17:34:31 +0200

    Multilanguage module 1

---
+++ lib/modules/multilanguage/MultiLanguage.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace pff\modules;
+
+/**
+ *
+ * @author paolo.fagni<at>gmail.com
+ */
+class MultiLanguage extends \pff\AModule implements \pff\IBeforeSystemHook
+{
+
+    /**
+     * Contains the user selected language if specified in URL or the default specified in the configuration
+     *
+     * @var string
+     */
+    private $_selectedLanguage;
+
+    /**
+     * Contains the specified default language, this could be overrider by $pffConfig['default_language']
+     *
+     * @var string
+     */
+    private $_defaultLang;
+
+    public function __construct($confFile = 'multilanguage/module.conf.yaml')
+    {
+        $moduleconfig = $this->readConfig($confFile);
+        $this->_loadConfig($moduleconfig);
+    }
+
+    /**
+     * Loads the configuration file
+     */
+    private function _loadConfig($parsedConfig)
+    {
+        $this->_defaultLang = $parsedConfig['moduleConf']['default_language'];
+    }
+
+    /**
+     * Executed before the system startup
+     *
+     * @return mixed
+     */
+    public function doBeforeSystem()
+    {
+        $url = $this->_app->getUrl();
+        $url = $this->processUrl($url);
+        $this->_app->setUrl($url);
+
+    }
+
+    public function processUrl($url)
+    {
+        $splittedUrl = explode('/', $url);
+        $langCodes = $this->getCodes();
+
+        if (isset($langCodes[$splittedUrl[0]])) {
+            $this->_selectedLanguage = array_shift($splittedUrl);
+            $processedUrl = implode('/', $splittedUrl);
+        } else {
+            if (isset($this->_app)) {
+                try {
+                    $this->_selectedLanguage = $this->_app->getConfig()->getConfigData('default_language');
+                } catch (\pff\ConfigException $e) {
+                    $this->_selectedLanguage = $this->_defaultLang;
+                }
+            } else {
+                $this->_selectedLanguage = $this->_defaultLang;
+            }
+
+            $processedUrl = $url;
+        }
+        return $processedUrl;
+    }
+
+    /**
+     * Returns an array with the language codes "lang_code" => "english_name"
+     *
+     * @return array
+     */
+    private function getCodes()
+    {
+        $languageCodes = array(
+            "aa" => "Afar",
+            "ab" => "Abkhazian",
+            "ae" => "Avestan",
+            "af" => "Afrikaans",
+            "ak" => "Akan",
+            "am" => "Amharic",
+            "an" => "Aragonese",
+            "ar" => "Arabic",
+            "as" => "Assamese",
+            "av" => "Avaric",
+            "ay" => "Aymara",
+            "az" => "Azerbaijani",
+            "ba" => "Bashkir",
+            "be" => "Belarusian",
+            "bg" => "Bulgarian",
+            "bh" => "Bihari",
+            "bi" => "Bislama",
+            "bm" => "Bambara",
+            "bn" => "Bengali",
+            "bo" => "Tibetan",
+            "br" => "Breton",
+            "bs" => "Bosnian",
+            "ca" => "Catalan",
+            "ce" => "Chechen",
+            "ch" => "Chamorro",
+            "co" => "Corsican",
+            "cr" => "Cree",
+            "cs" => "Czech",
+            "cu" => "Church Slavic",
+            "cv" => "Chuvash",
+            "cy" => "Welsh",
+            "da" => "Danish",
+            "de" => "German",
+            "dv" => "Divehi",
+            "dz" => "Dzongkha",
+            "ee" => "Ewe",
+            "el" => "Greek",
+            "en" => "English",
+            "eo" => "Esperanto",
+            "es" => "Spanish",
+            "et" => "Estonian",
+            "eu" => "Basque",
+            "fa" => "Persian",
+            "ff" => "Fulah",
+            "fi" => "Finnish",
+            "fj" => "Fijian",
+            "fo" => "Faroese",
+            "fr" => "French",
+            "fy" => "Western Frisian",
+            "ga" => "Irish",
+            "gd" => "Scottish Gaelic",
+            "gl" => "Galician",
+            "gn" => "Guarani",
+            "gu" => "Gujarati",
+            "gv" => "Manx",
+            "ha" => "Hausa",
+            "he" => "Hebrew",
+            "hi" => "Hindi",
+            "ho" => "Hiri Motu",
+            "hr" => "Croatian",
+            "ht" => "Haitian",
+            "hu" => "Hungarian",
+            "hy" => "Armenian",
+            "hz" => "Herero",
+            "ia" => "Interlingua (International Auxiliary Language Association)",
+            "id" => "Indonesian",
+            "ie" => "Interlingue",
+            "ig" => "Igbo",
+            "ii" => "Sichuan Yi",
+            "ik" => "Inupiaq",
+            "io" => "Ido",
+            "is" => "Icelandic",
+            "it" => "Italian",
+            "iu" => "Inuktitut",
+            "ja" => "Japanese",
+            "jv" => "Javanese",
+            "ka" => "Georgian",
+            "kg" => "Kongo",
+            "ki" => "Kikuyu",
+            "kj" => "Kwanyama",
+            "kk" => "Kazakh",
+            "kl" => "Kalaallisut",
+            "km" => "Khmer",
+            "kn" => "Kannada",
+            "ko" => "Korean",
+            "kr" => "Kanuri",
+            "ks" => "Kashmiri",
+            "ku" => "Kurdish",
+            "kv" => "Komi",
+            "kw" => "Cornish",
+            "ky" => "Kirghiz",
+            "la" => "Latin",
+            "lb" => "Luxembourgish",
+            "lg" => "Ganda",
+            "li" => "Limburgish",
+            "ln" => "Lingala",
+            "lo" => "Lao",
+            "lt" => "Lithuanian",
+            "lu" => "Luba-Katanga",
+            "lv" => "Latvian",
+            "mg" => "Malagasy",
+            "mh" => "Marshallese",
+            "mi" => "Maori",
+            "mk" => "Macedonian",
+            "ml" => "Malayalam",
+            "mn" => "Mongolian",
+            "mr" => "Marathi",
+            "ms" => "Malay",
+            "mt" => "Maltese",
+            "my" => "Burmese",
+            "na" => "Nauru",
+            "nb" => "Norwegian Bokmal",
+            "nd" => "North Ndebele",
+            "ne" => "Nepali",
+            "ng" => "Ndonga",
+            "nl" => "Dutch",
+            "nn" => "Norwegian Nynorsk",
+            "no" => "Norwegian",
+            "nr" => "South Ndebele",
+            "nv" => "Navajo",
+            "ny" => "Chichewa",
+            "oc" => "Occitan",
+            "oj" => "Ojibwa",
+            "om" => "Oromo",
+            "or" => "Oriya",
+            "os" => "Ossetian",
+            "pa" => "Panjabi",
+            "pi" => "Pali",
+            "pl" => "Polish",
+            "ps" => "Pashto",
+            "pt" => "Portuguese",
+            "qu" => "Quechua",
+            "rm" => "Raeto-Romance",
+            "rn" => "Kirundi",
+            "ro" => "Romanian",
+            "ru" => "Russian",
+            "rw" => "Kinyarwanda",
+            "sa" => "Sanskrit",
+            "sc" => "Sardinian",
+            "sd" => "Sindhi",
+            "se" => "Northern Sami",
+            "sg" => "Sango",
+            "si" => "Sinhala",
+            "sk" => "Slovak",
+            "sl" => "Slovenian",
+            "sm" => "Samoan",
+            "sn" => "Shona",
+            "so" => "Somali",
+            "sq" => "Albanian",
+            "sr" => "Serbian",
+            "ss" => "Swati",
+            "st" => "Southern Sotho",
+            "su" => "Sundanese",
+            "sv" => "Swedish",
+            "sw" => "Swahili",
+            "ta" => "Tamil",
+            "te" => "Telugu",
+            "tg" => "Tajik",
+            "th" => "Thai",
+            "ti" => "Tigrinya",
+            "tk" => "Turkmen",
+            "tl" => "Tagalog",
+            "tn" => "Tswana",
+            "to" => "Tonga",
+            "tr" => "Turkish",
+            "ts" => "Tsonga",
+            "tt" => "Tatar",
+            "tw" => "Twi",
+            "ty" => "Tahitian",
+            "ug" => "Uighur",
+            "uk" => "Ukrainian",
+            "ur" => "Urdu",
+            "uz" => "Uzbek",
+            "ve" => "Venda",
+            "vi" => "Vietnamese",
+            "vo" => "Volapuk",
+            "wa" => "Walloon",
+            "wo" => "Wolof",
+            "xh" => "Xhosa",
+            "yi" => "Yiddish",
+            "yo" => "Yoruba",
+            "za" => "Zhuang",
+            "zh" => "Chinese",
+            "zu" => "Zulu"
+        );
+        return $languageCodes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSelectedLanguage()
+    {
+        return $this->_selectedLanguage;
+    }
+}
---
+++ tests/modules/multilanguage/MultiLanguageTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @author paolo.fagni<at>gmail.com
+ */
+class MultiLanguageTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \pff\modules\MultiLanguage
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new \pff\modules\MultiLanguage();
+    }
+
+    public function testProcessUrlWithLanguage()
+    {
+        $tmpUrl = $this->object->processUrl('it/controller/action/param1');
+        $this->assertEquals('controller/action/param1', $tmpUrl);
+        $this->assertEquals('it', $this->object->getSelectedLanguage());
+    }
+
+    public function testProcessUrlWithNoLanguage()
+    {
+        $tmpUrl = $this->object->processUrl('controller/action/param1');
+        $this->assertEquals('controller/action/param1', $tmpUrl);
+    }
+}
