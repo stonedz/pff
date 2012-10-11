6c120ec943ed1a924fae488a66f85e2c152c4c15
Marco Sangiorgi <marco.sangiorgi@neropaco.net>
2012-10-11 17:34:46 +0200

    addedd mail module, based on SwiftMail

--- lib/modules/mail/Mail.php
+++ lib/modules/mail/Mail.php
@@ -3,25 +3,27 @@
 namespace pff\modules;
 
 /**
- * Helper module to manage cookies
+ * Helper module to send mail
  *
- * @author paolo.fagni<at>gmail.com
+ * @author marco.sangiorgi<at>neropaco.net
  */
+
+require_once(ROOT . DS . 'lib/vendor/swiftmailer/swiftmailer/lib/swift_init.php');
+
 class Mail extends \pff\AModule
 {
 
-    /**
-     * If true use encryped cookies
-     *
-     * @var bool
-     */
-    //private $_useEncryption;
-    private $phpmailer;
+    private $mailer;
+
+    private $transport;
+
+    private $message;
 
     public function __construct($confFile = 'mail/module.conf.yaml')
     {
-        $this->phpmailer = new \PHPMailerLite();
         $this->loadConfig($this->readConfig($confFile));
+
+        $this->mailer = new \Swift_Mailer($this->transport);
     }
 
     /**
@@ -31,11 +33,51 @@ class Mail extends \pff\AModule
      */
     private function loadConfig($parsedConfig)
     {
-        if(isset($parsedConfig['moduleConf']['Host']) && $parsedConfig['moduleConf']['Host'] != ""){
-            $this->phpmailer->Hostname = $parsedConfig['moduleConf']['Host'];
-        }
-        //$this->phpmailer->
-        //$this->_useEncryption = $parsedConfig['moduleConf']['useEncryption'];
+        if(isset($parsedConfig['moduleConf']['Type']) && $parsedConfig['moduleConf']['Type'] == "smtp"){
 
+            $this->transport = \Swift_SmtpTransport::newInstance();
+
+            if(isset($parsedConfig['moduleConf']['Host']) && $parsedConfig['moduleConf']['Host'] != ""){
+                $this->transport->setHost($parsedConfig['moduleConf']['Host']);
+            }
+
+            if(isset($parsedConfig['moduleConf']['Port']) && $parsedConfig['moduleConf']['Port'] != ""){
+                $this->transport->setPort($parsedConfig['moduleConf']['Port']);
+            }
+
+            if(isset($parsedConfig['moduleConf']['Username']) && $parsedConfig['moduleConf']['Username'] != ""){
+                $this->transport->setUsername($parsedConfig['moduleConf']['Username']);
+            }
+
+            if(isset($parsedConfig['moduleConf']['Password']) && $parsedConfig['moduleConf']['Password'] != ""){
+                $this->transport->setPassword($parsedConfig['moduleConf']['Password']);
+            }
+
+            if(isset($parsedConfig['moduleConf']['Encryption']) && $parsedConfig['moduleConf']['Encryption'] != ""){
+                $this->transport->setEncryption($parsedConfig['moduleConf']['Encryption']);
+            }
+
+        }
+        elseif(isset($parsedConfig['moduleConf']['Type']) && $parsedConfig['moduleConf']['Type'] == "sendmail"){
+
+            $this->transport = \Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+
+        }else{
+
+            $this->transport = \Swift_MailTransport::newInstance();
+
+        }
+
+    }
+
+    public function sendMail($to, $from, $fromName, $subject, $body, $attachment = null)
+    {
+        $this->message = new \Swift_Message();
+        $this->message->setTo($to);
+        $this->message->setFrom(array($from => $fromName));
+        $this->message->setSubject($subject);
+        $this->message->setBody($body);
+        $this->message->attach($attachment);
+        return $this->mailer->send($this->message);
     }
 }
