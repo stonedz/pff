6c0ab8c629cd97360c6ea71b1fa58165e45e62f6
Alessandro Rossi <alessandro.rossi@neropaco.net>
2013-04-19 18:59:13 +0200

    myversion

--- scripts/cli-config.php
+++ scripts/cli-config.php
@@ -54,3 +54,6 @@ $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
     'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
     'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
 ));
+
+$platform = $em->getConnection()->getDatabasePlatform();
+$platform->registerDoctrineTypeMapping('enum', 'string');
