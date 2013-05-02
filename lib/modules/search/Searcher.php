af7663428c5d7fa23bfe300939875bdaa0c80b44
Marco Sangiorgi <marco.sangiorgi@neropaco.net>
2013-05-02 12:09:13 +0200

    added searchDateRange to search module

--- lib/modules/search/Searcher.php
+++ lib/modules/search/Searcher.php
@@ -31,6 +31,7 @@ class Searcher extends \pff\AModule
      * @param $_em //the entity manager, needed to search on db
      * @param array $excludeArray //an array formed like $excludeArray['modelname']['field1, field2, ...]
      * @return array //returns an array formed like $return['modelname']['entity1', 'entity2'], ...]
+     * in case of daterange search, what must be an array formatted like [dateFrom, dateTo, fieldSeparator] (i.e.: ["03-03-2013", "07-03-2013", "-"])
      */
     public function search($what, $type, $_em, array $excludeArray = array()){
         if($what == ""){
@@ -51,6 +52,13 @@ class Searcher extends \pff\AModule
                         $return[$model] = $this->searchTextExact($what, $model, $_em, $excludeArray[$model]);
                     }else{
                         $return[$model] = $this->searchTextExact($what, $model, $_em);
+                    }
+                    break;
+                case "dateRange":
+                    if(isset($excludeArray[$model])){
+                        $return[$model] = $this->searchDateRange($what, $model, $_em, $excludeArray[$model]);
+                    }else{
+                        $return[$model] = $this->searchDateRange($what, $model, $_em);
                     }
                     break;
             }
@@ -121,6 +129,51 @@ class Searcher extends \pff\AModule
         foreach($searchableProperties as $prop){
             $or->add($qb->expr()->like("f.{$prop}", ":{$key}" ));
             $qb->setParameter($key, "{$what}");
+        }
+        $qb->where($or);
+        $results = $qb->getQuery()->getResult();
+        if(count($results) == 0){
+            return false;
+        }
+        return $results;
+    }
+
+    /**
+     * @param $what
+     * @param $modelname
+     * @param $_em
+     * @param array $excludeArray
+     * @return bool, array
+     * return an array of entity matching $what, false otherwise
+     */
+    private function searchDateRange($what, $modelname, $_em, $excludeArray = array()){
+        if(!is_array($what) && count($what) != 3){
+            return false;
+        }
+        $reflector = new \ReflectionClass("\pff\models\\".$modelname);
+        $properties = $this->my_class_type($reflector);
+        $searchableProperties = array();
+        foreach($properties as $key=>$value){
+            if(($value == "datetime" || $value == "date") &&  !in_array($key, $excludeArray)){
+                array_push($searchableProperties, $key);
+            }
+        }
+        if(count($searchableProperties) == 0){
+            return false;
+        }
+        $data_da = str_replace($what[2],"-",$what[0]);
+        $data_a = str_replace($what[2],"-",$what[1]);
+        $data_da = date("Y-m-d",strtotime($data_da));
+        $data_a = date("Y-m-d",strtotime($data_a));
+
+        $qb = $_em->createQueryBuilder();
+        $qb->select('f')
+            ->from("pff\models\\".$modelname, 'f');
+        $or = $qb->expr()->orx();
+        foreach($searchableProperties as $prop){
+            $or->add($qb->expr()->between("f.{$prop}", ":da", ":a" ));
+            $qb->setParameter("da", "{$data_da}");
+            $qb->setParameter("a", "{$data_a}");
         }
         $qb->where($or);
         $results = $qb->getQuery()->getResult();
