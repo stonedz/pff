648b776a406368328f4d75cc5438659d472ff321
Marco Bernini <marco.bernini@neropaco.net>
2013-06-10 11:00:25 +0200

    added session variable to mobile views module to load it always

--- scripts/generateAutoloadApp.sh
+++ scripts/generateAutoloadApp.sh
@@ -4,5 +4,7 @@ phpab --exclude 'app/vendor/doctrine/common/tests*' \
     --exclude 'app/vendor/doctrine/dbal/tests*' \
     --exclude 'app/vendor/doctrine/orm/tests*' \
     --exclude 'app/public/*' \
+	--exclude 'app/vendor/hybridauth/hybridauth/additional-providers/*' \
+	--exclude 'app/vendor/dompdf/dompdf/lib/php-font-lib/*' \
 	--output app/autoload.php app
 
