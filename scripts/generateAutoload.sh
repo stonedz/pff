dfd15714213d1ac899bbb1a1768a4b9b1427a49b
Paolo Fagni <paolo.fagni@gmail.com>
2013-11-14 14:45:10 +0100

    Updated generateAutoload and composer dependencies

--- scripts/generateAutoload.sh
+++ scripts/generateAutoload.sh
@@ -2,19 +2,21 @@
 
 cd ..
 phpab --exclude 'lib/vendor/ezyang/htmlpurifier/maintenance/*' \
-    --exclude 'lib/vendor/doctrine/cache/tests/*' \
-    --exclude 'lib/vendor/doctrine/annotations/tests/*' \
-    --exclude 'lib/vendor/doctrine/inflector/tests/*' \
     --exclude 'lib/vendor/doctrine/common/tests*' \
     --exclude 'lib/vendor/doctrine/dbal/tests*' \
     --exclude 'lib/vendor/doctrine/orm/tests*' \
     --exclude 'lib/vendor/smarty/smarty/development*' \
     --exclude 'lib/vendor/swiftmailer/swiftmailer/test-suite*' \
     --exclude 'lib/vendor/swiftmailer/swiftmailer/tests*' \
-        --output lib/autoload.php lib
+    --exclude '*/tests/*' \
+    --output lib/autoload.php lib
+
 phpab --output tests/autoload.php tests
+
 phpab --exclude 'app/vendor/doctrine/common/tests*' \
     --exclude 'app/vendor/doctrine/dbal/tests*' \
     --exclude 'app/vendor/doctrine/orm/tests*' \
     --exclude 'app/public/*' \
-	--output app/autoload.php app
+    --exclude 'app/vendor/hybridauth/hybridauth/additional-providers/*' \
+    --exclude 'app/vendor/dompdf/dompdf/lib/php-font-lib/*' \
+    --output app/autoload.php app
