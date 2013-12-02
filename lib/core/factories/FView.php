27df733dd9041abe1a34750ae0fa4df28a40bb37
Paolo Fagni <paolo.fagni@gmail.com>
2013-12-02 10:23:48 +0100

    Fixed renderAction bug with mobileviews

--- lib/core/factories/FView.php
+++ lib/core/factories/FView.php
@@ -21,21 +21,7 @@ class FView {
      */
     static public function create($templateName, \pff\App $app, $templateType = null) {
         $standardTemplate = $templateName;
-        $mm               = $app->getModuleManager();
 
-        if($mm->isLoaded('mobile_views')) {
-            /** @var \pff\modules\MobileViews $mobileViews */
-            $mobileViews = $mm->getModule('mobile_views');
-            if($mobileViews->isMobile() || $mobileViews->getMobileViewOnly()) {
-
-                $tmp          = explode('.', $templateName);
-                $tmp[0]      .= '_mobile';
-                $templateName = implode('.',$tmp);
-            }
-        }
-        else {
-            $mobileViews = null;
-        }
 
         if ($templateType === null) {
             $tmp          = explode('.', $templateName);
@@ -44,27 +30,73 @@ class FView {
             $templateType = strtolower($templateType);
         }
 
-        try {
-            return self::loadTemplate($templateName, $app, $templateType);
-        }
-        catch(\Exception $e) {
-            return self::loadTemplate($standardTemplate, $app, $templateType);
-        }
+        return self::loadTemplate($templateName, $app, $templateType);
+
+
+//        try {
+//            return self::loadTemplate($templateName, $app, $templateType);
+//        }
+//        catch(\Exception $e) {
+//            return self::loadTemplate($standardTemplate, $app, $templateType);
+//        }
     }
 
     static private function loadTemplate($templateName, \pff\App $app, $templateType) {
+        $mm = $app->getModuleManager();
+
         switch ($templateType) {
             case 'php':
+                $templateName = self::checkMobile($templateName, $mm, 'php');
                 return new \pff\ViewPHP($templateName, $app);
                 break;
             case 'tpl':
             case 'smarty':
+                $templateName = self::checkMobile($templateName, $mm, 'smarty');
                 return new \pff\ViewSmarty($templateName, $app);
                 break;
             default:
+                $templateName = self::checkMobile($templateName, $mm, 'php');
                 return new \pff\ViewPHP($templateName, $app);
                 break;
 
+        }
+    }
+
+    /**
+     * @param $templateName
+     * @param $mm
+     * @return array
+     */
+    private static function checkMobile($templateName, $mm, $type) {
+        if ($mm->isLoaded('mobile_views')) {
+
+            /** @var \pff\modules\MobileViews $mobileViews */
+            $mobileViews = $mm->getModule('mobile_views');
+            if ($mobileViews->isMobile() || $mobileViews->getMobileViewOnly()) {
+                $tmp = explode('.', $templateName);
+                $tmp[0] .= '_mobile';
+                $tempTemplateName = implode('.', $tmp);
+
+                if($type == 'php') {
+                    $templatePath = ROOT . DS . 'app' . DS . 'views' . DS .  $tempTemplateName;
+                }
+                else{ // smarty
+                    $templatePath = ROOT . DS . 'app' . DS . 'views' . DS . 'smarty' . DS . 'templates' . DS . $tempTemplateName;
+                }
+
+                if (file_exists($templatePath)) {
+                    return $tempTemplateName;
+                }
+                else {
+                    return $templateName;
+                }
+            }
+            else {
+                return $templateName;
+            }
+        }
+        else {
+            return $templateName;
         }
     }
 }
