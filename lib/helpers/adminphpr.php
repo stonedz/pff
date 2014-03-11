c927f011dde50b87e8d1fc92675706539ab8103c
Paolo Fagni <paolo.fagni@gmail.com>
2014-03-11 10:50:27 +0100

    added phprunner file name helper

--- lib/helpers/adminphpr.php
+++ lib/helpers/adminphpr.php
@@ -34,3 +34,19 @@ if (!function_exists("phpr_getThumbName")){
         return $value;
     }
 }
+
+if (!function_exists("phpr_getPdfName")){
+    /**
+     * Gets the pdf filename
+     *
+     * @param string $json
+     * @return string
+     */
+    function phpr_getPdfName($json) {
+        $arr        = json_decode($json);
+        $path_parts = pathinfo($arr[0]->name);
+
+        $value = $path_parts['filename'] . '.' . $path_parts['extension'];
+        return $value;
+    }
+}
